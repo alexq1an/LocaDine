97e7f1d0dd233a6702610d52039519eb50b25ced
Lihao Qian <85897966+alexq1an@users.noreply.github.com>
2023-11-18 15:50:27 -0800

    still MapsActivity small update

--- app/src/main/java/com/example/locadine/MapsActivity.kt
+++ app/src/main/java/com/example/locadine/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.example.locadine
 
+import android.app.Activity
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +16,17 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.locadine.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.Marker
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var  markerOptions: MarkerOptions
+    private lateinit var locationManager: LocationManager
+    private lateinit var findRestaurantButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +38,70 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Permission check
+        Util.checkPermissions(this)
+
+        findRestaurantButton = binding.findButton
+        findRestaurantButton.setOnClickListener(){
+            // TODO future find service
+
+
+        }
+
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        markerOptions = MarkerOptions()
+
+        initLocationManager() // initialize map
+
+    }
+
+    fun initLocationManager() {
+        try { // initialize steps
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                return
+            }
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (location != null)
+                onLocationChanged(location)
+            // update 1 second at a time
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0f, this)
+
+        } catch (e: SecurityException) { // catch no location permission exception
+            Toast.makeText(this, "No Location permission", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    // initialize marker for each iteration
+    private var lastMarker: Marker? = null
+    override fun onLocationChanged(location: Location) {
+        val lat = location.latitude
+        val lng = location.longitude
+        val latLng = LatLng(lat, lng)
+        // update camera position
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17f)
+        mMap.animateCamera(cameraUpdate)
+
+        lastMarker?.remove()
+        markerOptions.position(latLng)
+        // keep a reference to the last marker
+        lastMarker = mMap.addMarker(markerOptions)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (locationManager != null)
+            locationManager.removeUpdates(this)
     }
 }
--- app/src/main/java/com/example/locadine/Util.kt
+++ app/src/main/java/com/example/locadine/Util.kt
@@ -1,4 +1,32 @@
 package com.example.locadine
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 object Util {
+    fun checkPermissions(activity: Activity?) {
+        if (Build.VERSION.SDK_INT < 23) {
+            return
+        }
+        val permissionsToRequest = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        val permissionsNotGranted = permissionsToRequest.filter {
+            ContextCompat.checkSelfPermission(activity!!, it) != PackageManager.PERMISSION_GRANTED
+        }
+        if (permissionsNotGranted.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                activity!!,
+                permissionsNotGranted.toTypedArray(),
+                0
+            )
+        }
+    }
 }
